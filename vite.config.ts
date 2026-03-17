52b98754b5f3952ae55ebc33bb97ef25342e7e21
xwoxmati <73510570+xwoxmati@users.noreply.github.com>
2026-03-17 05:05:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/xwoxmati.github.io/",
+  base: "https://xwoxmati.github.io/",
   server: {
     host: "::",
     port: 8080,
